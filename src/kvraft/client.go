b0cee858ac173c39f9b1531534b0134d44c50796
nemohan <xyzlinux@163.com>
2018-03-23 19:16:05 +0800

    fill the framework

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -3,11 +3,14 @@ package raftkv
 import "labrpc"
 import "crypto/rand"
 import "math/big"
-
+//import "sync/atomic"
+//import "sync"
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	leader int
+	seq    int
 }
 
 func nrand() int64 {
@@ -21,6 +24,7 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.leader = -1
 	return ck
 }
 
@@ -37,9 +41,29 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) Get(key string) string {
-
 	// You will have to modify this function.
-	return ""
+	value := ""
+	seq := ck.seq
+	ck.seq++
+	arg := &GetArgs{Key : key, Seq : seq}
+	reply := &GetReply{}
+	if ck.leader != -1{
+		ok := ck.servers[ck.leader].Call("RaftKV.Get", arg, reply)
+		if !ok || reply.WrongLeader{
+			goto next
+		}
+		return reply.Value
+	}
+	next:
+	for i, c := range ck.servers{
+		ok := c.Call("RaftKV.Get", arg, reply)
+		if !ok || reply.WrongLeader{
+			continue
+		}
+		ck.leader = i
+		return reply.Value
+	}
+	return value
 }
 
 //
@@ -54,6 +78,29 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	seq := ck.seq
+	ck.seq++
+	arg := &PutAppendArgs{Key: key, Value: value, Op: op, Seq:seq}
+	reply := &PutAppendReply{}
+	if ck.leader != -1{
+		ok := ck.servers[ck.leader].Call("RaftKV.PutAppend", arg, reply)
+		if !ok || reply.WrongLeader{
+			goto next
+		}
+		if reply.Err == OK{
+			return
+		}
+	}
+	next:
+	for i, c := range ck.servers{
+		ok := c.Call("RaftKV.PutAppend", arg, reply)
+		if !ok || reply.WrongLeader{
+			continue
+		}
+		ck.leader = i
+		return
+	}
+
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -2,13 +2,17 @@ package raftkv
 
 import (
 	"encoding/gob"
+	"fmt"
 	"labrpc"
 	"log"
 	"raft"
 	"sync"
+	"time"
+	"os"
 )
 
-const Debug = 0
+//const Debug = 0
+const Debug =1
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
@@ -22,6 +26,24 @@ type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Value string
+	Key string
+	ID int
+	Cmd string
+	From int
+}
+
+type PendingOp struct{
+	ops map[int]*Op
+	notifyCh map[int] chan *Result
+}
+
+type Result struct{
+	Value string
+	Err string
+}
+type CMD struct{
+	resultChan chan *Result 
 }
 
 type RaftKV struct {
@@ -33,17 +55,185 @@ type RaftKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	commited  map[string] *Op
+	//pending   map[int] *Op
+	pending  map[int]*PendingOp // key is client id
+	clientID map[int]int
+	log *log.Logger
+	cmdCh chan *CMD
 }
 
+func (kv *RaftKV) Log(format string, arg ...interface{}){
+	if Debug > 0{
+		kv.log.Printf(format, arg...)
+	}
+}
 
+func (op *Op) String()string{
+	return fmt.Sprintf("key:%s value:%v id:%d from:%d cmd:%s", op.Key, op.Value, op.ID, op.From, op.Cmd)
+}
 func (kv *RaftKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	from := args.From
+	kv.mu.Lock()
+	max, ok := kv.clientID[from]
+	//TODO: how to handle the return value properly
+	if ok && args.Seq <= max{
+		kv.mu.Unlock()
+		return
+	}
+	kv.mu.Unlock()
+	 op := Op{
+		Key : args.Key,
+		ID: args.Seq,
+		From: args.From,
+		Cmd: "Get",
+	}
+	_, _, isLeader := kv.rf.Start(op)
+	if !isLeader{
+		reply.WrongLeader = true
+		return
+	}
+	resultChan := kv.addPending(&op)
+	result := <-resultChan
+	reply.Value= result.Value
+	reply.From = op.From
+	reply.Seq = args.Seq
+	reply.Err = OK
 }
+
 
 func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.Log("putappend: args:%s\n", args.String())
+	from := args.From
+	kv.mu.Lock()
+	max, ok := kv.clientID[from]
+	//TODO: how to handle the return value properly
+	if ok && args.Seq <= max{
+		kv.mu.Unlock()
+		return
+	}
+	kv.mu.Unlock()
+	 op := Op{
+		Key : args.Key,
+		Value: args.Value,
+		ID: args.Seq,
+		From: args.From,
+		Cmd: args.Op,
+	}
+
+	num := 0
+	retry:
+	_, _, isLeader := kv.rf.Start(op)
+	if !isLeader{
+		if num < 3{
+			num++
+			time.Sleep(time.Millisecond * 500)
+			goto retry
+		}
+		reply.WrongLeader = true
+		kv.Log("not leader\n")
+		return
+	}
+	ch := kv.addPending(&op)
+	result := <-ch
+	reply.Seq = args.Seq
+	reply.Err = Err(result.Err)
 }
 
+func (kv *RaftKV) addPending(op *Op) chan *Result{
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	from := op.From
+	kv.clientID[from] = op.ID
+	pending, ok := kv.pending[from]
+	if !ok{
+		pending = &PendingOp{ ops: make(map[int]*Op), notifyCh: make(map[int]chan *Result)}
+		kv.pending[from] = pending
+	}
+	pending.ops[op.ID] = op
+	resultChan := make(chan *Result, 1)
+	pending.notifyCh[op.ID] = resultChan
+	return resultChan
+}
+
+func (kv *RaftKV) removePending(op *Op) chan *Result{
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.Log("remove op:%v\n", *op)
+	from := op.From
+	pending, ok:= kv.pending[from]
+	//We may not the leader
+	if !ok{
+		//kv.doPanic(op.String())
+		return nil
+	}
+	ch, ok := pending.notifyCh[op.ID]
+	if !ok{
+		kv.doPanic(op.String())
+	}
+	delete(pending.notifyCh, op.ID)
+	delete(pending.ops, op.ID)
+	return ch
+}
+
+
+func (kv *RaftKV) doPanic(info string){
+	panic(info)
+}
+
+func (kv *RaftKV) timeout(){
+
+}
+func (kv *RaftKV) poll(){
+	for{
+		select {
+			case msg := <-kv.applyCh:
+				op := msg.Command.(Op)
+				ch := kv.removePending(&op)
+				if ch == nil{
+					break
+				}
+				result := &Result{}
+				kv.mu.Lock()
+				switch op.Cmd{
+				case "Put":
+					_, ok := kv.commited[op.Key]
+					if !ok{
+						kv.commited[op.Key] = &op
+					}
+					kv.commited[op.Key].Value = op.Value
+					kv.Log("put: arg:%s\n", op.String())
+					result.Err = OK
+				case "Append":
+					_, ok := kv.commited[op.Key]
+					if !ok{
+						kv.commited[op.Key] = &op
+					}
+					oldV := kv.commited[op.Key].Value
+					newV := oldV + op.Value
+					kv.commited[op.Key].Value = newV
+					kv.Log("append: arg:%s new:%s\n", op.String(), newV) 
+					result.Err = OK
+				case "Get":
+					v, ok := kv.commited[op.Key]
+					if !ok{
+						kv.Log("get: arg:%s not find\n", op.String())
+						result.Err = ErrNoKey
+						break
+					}
+					result.Err = OK
+					result.Value = v.Value
+					kv.Log("get:arg:%s, result:%s\n", op.String(), v.Value)
+				}
+				kv.mu.Unlock()
+				ch <-result
+			default:
+				time.Sleep(time.Millisecond * 10)
+		}
+	}
+}
 //
 // the tester calls Kill() when a RaftKV instance won't
 // be needed again. you are not required to do anything
@@ -78,10 +268,16 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.maxraftstate = maxraftstate
 
 	// Your initialization code here.
+	kv.commited = make(map[string]*Op, 0)
+	kv.pending = make(map[int]*PendingOp, 0)
+	kv.clientID = make(map[int]int, 0)
+	kv.cmdCh = make(chan *CMD, 64)
+	prefix := fmt.Sprintf("[%05d] ", me)
+	kv.log = log.New(os.Stdout, prefix, log.Ldate | log.Lshortfile | log.Lmicroseconds)
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
-
+	go kv.poll()
 
 	return kv
 }
